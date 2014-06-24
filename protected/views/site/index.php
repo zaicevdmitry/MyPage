d1eb8991408d864adf00798686e53f943e607350
Dmitry Zaicev <dmitry.zaicev@intexsoft.by>
2014-06-24 16:45:30 +0300

    create page

---
+++ protected/views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Welcome!!!</i></h1>
+
+<ul>
+    <li>Привет</li>
+    <li>Hello</li>
+    <li><code><?php echo 'Это тест' ?></code></li>
+</ul>
